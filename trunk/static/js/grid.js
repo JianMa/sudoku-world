37cf84b60307dbb1643811d06987ff62f8c956c6
ma86jian <ma86jian@fd18d4f1-715e-3d08-666e-96644cb89c7e>
2011-01-14 04:10:07 +0000

    added ValidateGrid for grid.js loading speed is really slow

--- trunk/static/js/grid.js
+++ trunk/static/js/grid.js
@@ -1,46 +1,122 @@
 // Javascript code
-gridN = 9
+
+/*
+ * This file contains code that implements the handlers for an editable grid
+ * Handlers contained:
+ *     arrow key controls
+ *     validate the gird
+ */
+
+var GridN = 9
+
+function Location(x, y) {
+	this.x = x;
+	this.y = y;
+}
+
+function Td(type, x, y) {
+	return "td#" + type + x + y;
+}
+
+function TdInput(type, x, y) {
+	return Td(type, x, y) + " input";
+}
 
 $(document).ready(function() {
-	$("div.grid td input").keydown(function(e) {
-		var id = $(this).attr("id");
-		var type = id.charAt(0);
-		var loc = {"x" : parseInt(id.charAt(1)), "y" : parseInt(id.charAt(2))};
-		
-		switch (e.which) {
-			case 37:	// left
-				loc = move(loc, 0, -1);
-				break;
+	$("div.grid.editable").each(function(grid_index, grid) {
+		$("td", grid).each(function(td_index, td) {
+			var id = $(td).attr("id");
+			var type = id.charAt(0);
+			var loc = new Location(parseInt(id.charAt(1)), parseInt(id.charAt(2)));
+			
+			$("input", td).keydown(function(e) {
+				switch (e.which) {
+					case 37:	// left
+						MoveInBoundary(loc, 0, -1);
+						break;
+						
+					case 38:	// up
+						MoveInBoundary(loc, -1, 0);
+						break;
+						
+					case 39:	// right
+						MoveInBoundary(loc, 0, 1);
+						break;
+						
+					case 40:	// down
+						MoveInBoundary(loc, 1, 0);
+						break;
+						
+					default:
+						return;
+				}
 				
-			case 38:	// up
-				loc = move(loc, -1, 0);
-				break;
-				
-			case 39:	// right
-				loc = move(loc, 0, 1);
-				break;
-				
-			case 40:	// down
-				loc = move(loc, 1, 0);
-				break;
-				
-			default:
-				return;
-		}
-		
-		e.preventDefault();
-		var tbody = $(this).parent().parent().parent();		// input => td => tr => tbody
-		var td = "td input#" + type + loc["x"] + loc["y"];
-		
-		$(td, tbody).select();
+				e.preventDefault();
+				$(TdInput(type, loc.x, loc.y), grid).select();
+			}).keyup(function(e) {
+				ValidateGrid(grid, type);
+			});
+			ValidateGrid(grid, type);
+		});
 	});
 });
 
-function check(v) {
-	return 0 <= v && v < gridN;
+function IsValidLoc(v) {
+	return 0 <= v && v < GridN;
 }
 
-function move(loc, dx, dy) {
-	var res = {"x" : loc["x"] + dx, "y" : loc["y"] + dy};	
-	return (check(res["x"]) && check(res["y"])) ? res : loc;
+function MoveInBoundary(loc, dx, dy) {
+	if (IsValidLoc(loc.x + dx) && IsValidLoc(loc.y + dy)) {
+		loc.x += dx;
+		loc.y += dy;
+	}
+}
+
+function IsValidForRow(grid, type, x, y, value) {
+	for (j = 0; j < GridN; j++)
+		if (j != y && $(TdInput(type, x, j), grid).val() == value)
+			return false;
+	return true;
+}
+
+function IsValidForCol(grid, type, x, y, value) {
+	for (i = 0; i < GridN; i++)
+		if (i != x && $(TdInput(type, i, y), grid).val() == value)
+			return false;
+	return true;
+}
+
+function IsValidForSquare(grid, type, x, y, value) {
+	var si = parseInt(x / 3) * 3;
+	var sj = parseInt(y / 3) * 3;
+	
+	for (i = si; i < si + 3; i++)
+	for (j = sj; j < sj + 3; j++)
+		if ((i != x || j != y) && $(TdInput(type, i, j), grid).val() == value)
+			return false;
+	return true;
+}
+
+function IsValidCell(grid, type, x, y, value) {
+	if (value == "")
+		return true;
+	else
+		return	IsValidForRow(grid, type, x, y, value) &&
+				IsValidForCol(grid, type, x, y, value) &&
+				IsValidForSquare(grid, type, x, y, value);
+}
+
+function ValidateGrid(grid, type) {
+	$("td.conflict", grid).removeClass("conflict");
+	
+	var valid = true;
+	for (x = 0; x < GridN; x++)
+	for (y = 0; y < GridN; y++) {
+		var value = $(TdInput(type, x, y), grid).val();
+		if (!IsValidCell(grid, type, x, y, value)) {
+			$(Td(type, x, y), grid).addClass("conflict");
+			valid = false;
+		}
+	}
+	return valid;
 }
--- trunk/static/js/replace.js
+++ trunk/static/js/replace.js
@@ -1,26 +1,28 @@
 // Javascript code
 
+// TdInput is defined in grid.js
+
 var map;
 $(document).ready(function() {
 	$("#submit").click(function() {
 		// get map
 		map = new Array();
-		for (i = 1; i < gridN + 1; i++) {
+		for (i = 1; i < GridN + 1; i++) {
 			var key = i.toString();
-			var value = $("#map input#" + i).val();
+			var value = $("div#map input#" + i).val();
 			if (value != "")
 				map[key] = value;
 		}
 		
 		// map the original to target
-		for (i = 0; i < gridN; i++)
-			for (j = 0; j < gridN; j++) {
-				var key = $("#puzzle input#p" + i + j).val();
+		for (i = 0; i < GridN; i++)
+			for (j = 0; j < GridN; j++) {
+				var key = $("div#puzzle " + TdInput("p", i, j)).val();
 				
 				if (key in map)
-					$("#solution input#s" + i + j).val(map[key]);
+					$("div#solution " + TdInput("s", i, j)).val(map[key]);
 				else
-					$("#solution input#s" + i + j).val(key);
+					$("div#solution " + TdInput("s", i, j)).val(key);
 			}
 	});
 });
