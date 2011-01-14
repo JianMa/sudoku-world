9433989dcf3c535257103a3b82722d7be4c234b2
ma86jian <ma86jian@fd18d4f1-715e-3d08-666e-96644cb89c7e>
2011-01-14 22:04:47 +0000

    make demo work todo:     slow down the demo speed     clean it up

--- static/js/demo.js
+++ static/js/demo.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 	$("input#demo").click(function() {
 		InitializeGrid();
 		CopyPuzzleToDemo();
-		if (ValidateGrid($("div#puzzle"))) {
+		if (ValidateGrid($("div#puzzle"), "p")) {
 			Demo();
 		}
 	});
@@ -18,7 +18,7 @@ function InitializeGrid() {
 	for (i = 0; i < GridN; i++) {
 		grid[i] = new Array();
 		for (j = 0; j < GridN; j++) {
-			var p = "div#puzzle table " + TdInput("p", i, j);
+			var p = "div#puzzle " + TdInput("p", i, j);
 			grid[i][j] = parseInt($(p).val());
 			if (isNaN(grid[i][j]) || grid[i][j] < 1 || grid[i][j] > 9)
 				grid[i][j] = 0;
@@ -29,11 +29,15 @@ function InitializeGrid() {
 function CopyPuzzleToDemo() {
 	for (i = 0; i < GridN; i++)
 	for (j = 0; j < GridN; j++) {
-		var d = "div#demo table " + TdInput("d", i, j);
-		if (grid[i][j] != 0)
-			$(d).val(grid[i][j]);
-		else
-			$(d).val("");
+		var td = "div#demo " + Td("d", i, j);
+		var tdInput = "div#demo " + TdInput("d", i, j);
+		if (grid[i][j] != 0) {
+			$(tdInput).val(grid[i][j]);
+			$(td).addClass("const");
+		} else {
+			$(tdInput).val("");
+			$(td).removeClass("const");
+		}
 	}
 }
 /*
@@ -105,6 +109,85 @@ function IsValidPuzzle() {
 	return IsValidRow() && IsValidCol() && IsValidSquare();
 }*/
 
+function ValidateDemoForRow(x, value) {
+	var result = true;
+	for (j = 0; j < GridN; j++)
+		if (j != y && grid[x][j] == value) {
+			$("div#demo " + Td("d", x, j)).addClass("conflict");
+			result = false;
+		}
+	return result;
+}
+
+function ValidateDemoForCol(y, value) {
+	var result = true;
+	for (i = 0; i < GridN; i++)
+		if (i != x && grid[i][y] == value) {
+			$("div#demo " + Td("d", i, y)).addClass("conflict");
+			result = false;
+		}
+	return result;
+}
+
+function ValidateDemoForSquare(x, y, value) {
+	var result = true;
+	var si = parseInt(x / 3) * 3;
+	var sj = parseInt(y / 3) * 3;
+	
+	for (i = si; i < si + 3; i++)
+	for (j = sj; j < sj + 3; j++)
+		if ((i != x || j != y) && grid[i][j] == value) {
+			$("div#demo " + Td("d", i, j)).addClass("conflict");
+			result = false;
+		}
+	return result;
+}
+
+function ValidateDemo(x, y, value) {
+	$("div#demo td.conflict").removeClass("conflict");
+	
+	var isValidRow = ValidateDemoForRow(x, value);
+	var isValidCol = ValidateDemoForCol(y, value);
+	var isValidSquare = ValidateDemoForSquare(x, y, value);
+	
+	return isValidRow && isValidCol && isValidSquare;
+}
+
+function Do(x, y) {
+	var result = false;
+	while (grid[x][y] != 0) {
+		if (x == GridN - 1 && y == GridN - 1) {
+			return true;
+		} else {
+			y += 1;
+			if (y == GridN) {
+				x += 1;
+				y = 0;
+			}
+		}
+	}
+	
+	var value;
+	for (value = 1; value < GridN + 1; value++)
+		if (ValidateDemo(x, y, value)) {
+			$("div#demo " + TdInput("d", x, y)).val(value);
+			//alert(value);
+			grid[x][y] = value;
+			result = result || Do(x, y)
+			if (result)
+				break;
+			$("div#demo " + TdInput("d", x, y)).val("");
+			grid[x][y] = 0;
+		} else
+			$("div#demo " + Td("d", x, y)).addClass("conflict");
+	return result;
+}
+
 function Demo() {
+	// avoid double-clicking the Demo button during demonstrating
 	$("input#demo").val("Demonstrating").attr("disabled", "disabled");
+	
+	Do(0, 0);
+	
+	$("input#demo").val("Demo").removeAttr("disabled");
 }
