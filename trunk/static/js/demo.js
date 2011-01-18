d961318b4d3f8275014b7f84b91f2eaeb4f72933
ma86jian <ma86jian@fd18d4f1-715e-3d08-666e-96644cb89c7e>
2011-01-18 22:47:37 +0000

    cleaned up the code so that Fgrid is a class, Demo is a class finished Demo function added a button to empty a grid todo:   control Demo speed

--- trunk/static/js/demo.js
+++ trunk/static/js/demo.js
@@ -1,124 +1,73 @@
 // Javascript Document
 
-// Grid, TdInput, Location is defined in grid.js
-
-function ValidateRow(fgrid, grid, x, y, value) {
-	var valid = true;
-	for (j = 0; j < GridN; j++)
-		if (j != y && grid[x][j].value == value) {
-			fgrid.jTd(x, j).conflict();
-			valid = false;
-		}
-	return valid;
+DemoStatus = {
+	Error:		0,
+	Ready:		1,
+	Running:	2,
+	Pausing:	3
 }
-
-function ValidateCol(fgrid, grid, x, y, value) {
-	var valid = true;
-	for (i = 0; i < GridN; i++)
-		if (i != x && grid[i][y].value == value) {
-			fgrid.jTd(i, y).conflict();
-			valid = false;
-		}
-	return valid;
-}
-
-function ValidateSquare(fgrid, grid, x, y, value) {
-	var valid = true;
-	var si = parseInt(x / 3) * 3;
-	var sj = parseInt(y / 3) * 3;
-	
-	for (i = si; i < si + 3; i++)
-	for (j = sj; j < sj + 3; j++)
-		if ((i != x || j != y) && grid[i][j].value == value) {
-			fgrid.jTd(i, j).conflict();
-			valid = false;
-		}
-	return valid;
-}
-
-function ValidateGrid(fgrid, grid, x, y, value) {
-	var valid =	ValidateRow(fgrid, grid, x, y, value) &&
-				ValidateCol(fgrid, grid, x, y, value) &&
-				ValidateSquare(fgrid, grid, x, y, value);
-	if (!valid)
-		fgrid.jTd(x, y).conflict();
-	return valid;
-}
-
-
-// class Cell: represents a cell in the grid
-function Cell(value, preset) {
-	this.value = value;
-	this.preset = preset;
-}
-
 
 // class Demo: handles demonstration
 function Demo(inFgrid, outFgrid) {
 	this.inFgrid = inFgrid;
 	this.outFgrid = outFgrid;
 	
-	this.MovePrev = MovePrev;
-	this.MoveNext = MoveNext;
-	// inner grid
-	this.grid = new Array();
-	this.loc = new Location(0, 0);
+	this.ResetLoc();
 	
 	// timer
 	this.interval = 500;
 	this.timer = null;
-	
-	for (i = 0; i < GridN; i++) {
-		this.grid[i] = new Array();
-		for (j = 0; j < GridN; j++) {
-			var value = parseInt(this.inFgrid.jTdInput(i, j).val());
-			if (isNaN(value) || value < 1 || value > 9)
-				this.grid[i][j] = new Cell(0, false);
-			else
-				this.grid[i][j] = new Cell(value, true);
+}
+
+// move loc to last non-preset cell location
+Demo.prototype.MovePrev = function(loc) {
+	do {
+		loc.y--;
+		if (loc.y < 0) {
+			loc.x--;
+			loc.y = GridN - 1;
 		}
-	}
-	if (this.grid[0][0].preset)
-		this.initial = !this.MoveNext(this.loc);
-	else
-		this.initial = false;
-	
-	
-	// move loc to last non-preset cell location
-	function MovePrev(loc) {
-		do {
-			loc.y--;
-			if (loc.y < 0) {
-				loc.x--;
-				loc.y = GridN - 1;
-			}
-			if (loc.x < 0)
-				return false;
-		} while(this.grid[loc.x][loc.y].preset);
-		return true;
-	}
-	
-	// move loc to next non-preset cell location
-	function MoveNext(loc) {
-		do {
-			loc.y++;
-			if (loc.y >= GridN) {
-				loc.x++;
-				loc.y = 0;
-			}
-			if (loc.x >= GridN)
-				return false;
-		} while(this.grid[loc.x][loc.y].preset);
-		return true;
+		if (loc.x < 0)
+			return false;
+	} while(this.IsPreset(loc.x, loc.y));
+	return true;
+}
+
+// move loc to next non-preset cell location
+Demo.prototype.MoveNext = function(loc) {
+	do {
+		loc.y++;
+		if (loc.y >= GridN) {
+			loc.x++;
+			loc.y = 0;
+		}
+		if (loc.x >= GridN)
+			return false;
+	} while(this.IsPreset(loc.x, loc.y));
+	return true;
+}
+
+Demo.prototype.IsPreset = function(x, y) {
+	return this.inFgrid.value(x, y) != "";
+}
+
+Demo.prototype.ResetLoc = function() {
+	this.loc = new Location(0, 0);
+	if (this.IsPreset(0, 0))
+		this.MoveNext(this.loc);
+}
+
+Demo.prototype.CopyToOut = function() {
+	for (var i = 0; i < GridN; i++)
+	for (var j = 0; j < GridN; j++) {
+		var value = this.inFgrid.value(i, j);
+		this.outFgrid.value(i, j, value);
 	}
 }
 
-
-function IsValidLoc(loc) {
-	return 0 <= loc && loc < GridN;
+Demo.prototype.IsSuccessful = function() {
+	return this.loc.x == GridN && this.loc.y == 0;
 }
-
-
 
 // returning false means end (it can be either successful or unsuceessful)
 // returning true means a try
@@ -126,48 +75,38 @@ Demo.prototype.Next = function() {
 	if (!IsValidLoc(this.loc.x) || !IsValidLoc(this.loc.y))
 		return false;
 	
-	var cell = this.grid[this.loc.x][this.loc.y];
-	cell.value = (cell.value + 1) % (GridN + 1);
+	var value = this.outFgrid.value(this.loc.x, this.loc.y);
+	value =  (value + 1) % (GridN + 1);					// try next value
+	this.outFgrid.value(this.loc.x, this.loc.y, value);
+	this.outFgrid.resolved();
 	
-	$("td.conflict", this.outJgrid).resolved();
-	this.outFgrid.SetValue(this.loc.x, this.loc.y, cell.value);
-	
-	if (cell.value == 0)
+	if (value == 0)
 		return this.MovePrev(this.loc);
 	else {
-		if (!ValidateGrid(this.outFgrid, this.grid, this.loc.x, this.loc.y, cell.value))
+		if (!this.outFgrid.ValidateCell(this.loc.x, this.loc.y))
 			return true;
 		else
 			return this.MoveNext(this.loc);
 	}
 }
 
-function run(demo, callback) {
-	if (demo.Next()) {
-		demo.timer = setTimeout(function() {
-			run(demo, callback);
-		}, demo.interval);
+// callback is invoked when running is finished
+Demo.prototype.Run = function(callback) {
+	var demo = this;
+	if (this.Next()) {
+		this.timer = setTimeout(function() {
+			demo.Run(callback);
+		}, this.interval);
 	} else
-		callback();
-}
-
-Demo.prototype.Start = function(callback) {
-	run(this, callback);
+		if (typeof callback == "function")
+			callback(this, this.IsSuccessful());
 }
 
 Demo.prototype.Stop = function() {
 	clearTimeout(this.timer);
 }
 
-Demo.prototype.Pause = function() {
-	clearTimeout(this.timer);
-}
-
-Demo.prototype.Continue = function(callback) {
-	run(this, callback);
-}
-
-function DemoController(enableStart, enableStop, enablePause, enableContinue) {
+Demo.prototype.Controller = function(enableStart, enableStop, enablePause, enableContinue) {
 	if (enableStart)
 		$("input#start").removeAttr("disabled");
 	else
@@ -189,44 +128,70 @@ function DemoController(enableStart, enableStop, enablePause, enableContinue) {
 		$("input#continue").attr("disabled", "disabled");
 }
 
-
-function end() {
-	DemoController(true, false, false, false);
-	alert("finished demo!");
+Demo.prototype.ChangeStatus = function(status) {
+	switch(status) {
+		case DemoStatus.Error:
+			$("div#status span#name").text("Error");
+			this.Controller(false, false, false, false);
+			break;
+			
+		case DemoStatus.Ready:
+			$("div#status span#name").text("Ready");
+			this.Controller(true, false, false, false);
+			this.Stop();
+			this.ResetLoc();
+			break;
+			
+		case DemoStatus.Running:
+			$("div#status span#name").text("Running");
+			this.Controller(false, true, true, false);
+			this.Run(finish);
+			break;
+			
+		case DemoStatus.Pausing:
+			$("div#status span#name").text("Pausing");
+			this.Controller(false, true, false, true);
+			this.Stop();
+			break;
+	}
+	$("div#status span#result").text("");
 }
 
-var demo = null;
-$(document).ready(function() {
-	var inFgrid = null;
-	var outFgrid = null;
+function finish(demo, successful) {
+	demo.ChangeStatus(DemoStatus.Ready);
 	
-	DemoController(true, false, false, false);
-	// demo should listen to ValidateJgrid change
-	// button should be disabled if ValidateJgrid is false
+	if (successful)
+		$("div#status span#result").text("It succeeded to find a solution!");
+	else
+		$("div#status span#result").text(result = "It failed to find a solution!");
+}
+
+
+$(document).ready(function() {
+	var inFgrid = new Fgrid($("div#puzzle"), "p");
+	var outFgrid = new Fgrid($("div#demo"), "d");
+	var demo = new Demo(inFgrid, outFgrid);
+	
+	inFgrid.RegisterEditable(function(valid) {
+		demo.ChangeStatus(valid ? DemoStatus.Ready : DemoStatus.Error);
+	});
+	outFgrid.Register();
+	
+	
 	$("input#start").click(function() {
-		DemoController(true, true, true, false);
-		
-		inFgrid = new Fgrid($("div#puzzle"), "p", null);
-		if (!ValidateJgrid($("div#puzzle"), "p"))
-			return;
-		outFgrid = new Fgrid($("div#demo"), "d", inFgrid.grid);
-		
-		demo = new Demo(inFgrid, outFgrid);
-		demo.Start(end);
+		demo.CopyToOut();
+		demo.ChangeStatus(DemoStatus.Running);
 	});
 	
 	$("input#stop").click(function() {
-		DemoController(true, false, false, false);
-		demo.Stop();
+		demo.ChangeStatus(DemoStatus.Ready);
 	});
 	
 	$("input#pause").click(function() {
-		DemoController(true, true, false, true);
-		demo.Pause();
+		demo.ChangeStatus(DemoStatus.Pausing);
 	});
 	
 	$("input#continue").click(function() {
-		DemoController(true, true, true, false);
-		demo.Continue(end);
+		demo.ChangeStatus(DemoStatus.Running);
 	});
 });
--- trunk/static/js/grid.js
+++ trunk/static/js/grid.js
@@ -6,137 +6,16 @@
  *     arrow key controls
  *     validate the gird
  * Convention:
+ *     fgrid:	a grid class containing jgrid and type information
  *     jgrid:	a div grid in jQuery wrap, like $("div#puzzle")
  *     grid:	a 2-dimension array grid, like grid[0][0] is a cell
  */
 
 var GridN = 9
 
-function IsValidValue(value) {
-	return !isNaN(value) && 1 <= value && value <= GridN;
+function IsValidValue(v) {
+	return !isNaN(v) && 1 <= v && v <= GridN;
 }
-
-// class Location
-function Location(x, y) {
-	this.x = x;
-	this.y = y;
-}
-
-// class Fgrid (full information grid)
-function Fgrid(jgrid, type, grid){
-	// define method
-	this.SetValue = function(x, y, value) {
-		if (IsValidValue(value)) {
-			this.jTdInput(x, y).val(value);
-			this.grid[x][y] = value;
-		} else {
-			this.jTdInput(x, y).val("");	
-			this.grid[x][y] = 0;
-		}
-	}
-	
-	this.jTd = function(x, y) {
-		return $("td#" + this.type + x + y, this.jgrid);
-	}
-	
-	this.jTdInput = function(x, y) {
-		return $("input", this.jTd(x, y));
-	}
-	
-	// initialization
-	this.jgrid = jgrid;
-	this.type = type;
-	this.grid = grid;
-	
-	if (grid == null) {
-		// initialize grid by jgrid
-		this.grid = new Array();
-		
-		for (i = 0; i < GridN; i++) {
-			this.grid[i] = new Array();
-			for (j = 0; j < GridN; j++) {
-				var value = parseInt(this.jTdInput(i, j).val());
-				if (IsValidValue(value))
-					this.grid[i][j] = new Cell(value, true);
-				else
-					this.grid[i][j] = new Cell(0, false);	
-			}
-		}
-	} else {
-		// copy grid to jgrid
-		for (i = 0; i < GridN; i++)
-		for (j = 0; j < GridN; j++) {
-			var value = this.grid[i][j].value;
-			if (IsValidValue(value))
-				this.jTdInput(i, j).val(value);
-			else
-				this.jTdInput(i, j).val("");
-		}
-	}
-}
-
-
-// clean them up
-function Td(type, x, y) {
-	return "td#" + type + x + y;
-}
-
-function TdInput(type, x, y) {
-	return Td(type, x, y) + " input";
-}
-
-$.fn.conflict = function() {
-	this.addClass("conflict");
-}
-
-$.fn.resolved = function() {
-	this.removeClass("conflict");
-}
-
-$(document).ready(function() {
-	$("div.grid.editable").each(function(dGrid_index, jgrid) {
-		$("td.cell", jgrid).each(function(td_index, td) {
-			// should use change handler
-			$("input", td).keydown(function(e) {
-				var id = $(td).attr("id");
-				var type = id.charAt(0);
-				var loc = new Location(parseInt(id.charAt(1)), parseInt(id.charAt(2)));
-				
-				$(TdInput(type, loc.x, loc.y), jgrid).blur();
-				switch (e.which) {
-					case 37:	// left
-						MoveInBoundary(loc, 0, -1);
-						break;
-						
-					case 38:	// up
-						MoveInBoundary(loc, -1, 0);
-						break;
-						
-					case 39:	// right
-						MoveInBoundary(loc, 0, 1);
-						break;
-						
-					case 40:	// down
-						MoveInBoundary(loc, 1, 0);
-						break;
-						
-					default:
-						$(TdInput(type, loc.x, loc.y), jgrid).focus();
-						return;
-				}
-				e.preventDefault();
-				$(TdInput(type, loc.x, loc.y), jgrid).focus();
-			}).keyup(function(e) {
-				$(this).focus();
-				ValidateJgrid(jgrid, type);
-			}).focus(function(e) {
-				$(this).select();
-			});
-		});
-		var type = $("td.cell", jgrid).attr("id").charAt(0);
-		ValidateJgrid(jgrid, type);
-	});
-});
 
 function IsValidLoc(v) {
 	return 0 <= v && v < GridN;
@@ -149,51 +28,171 @@ function MoveInBoundary(loc, dx, dy) {
 	}
 }
 
-function IsValidForRow(jgrid, type, x, y, value) {
-	for (j = 0; j < GridN; j++)
-		if (j != y && $(TdInput(type, x, j), jgrid).val() == value)
-			return false;
-	return true;
+$.fn.conflict = function() {
+	this.addClass("conflict");
 }
 
-function IsValidForCol(jgrid, type, x, y, value) {
-	for (i = 0; i < GridN; i++)
-		if (i != x && $(TdInput(type, i, y), jgrid).val() == value)
-			return false;
-	return true;
+$.fn.resolved = function() {
+	this.removeClass("conflict");
 }
 
-function IsValidForSquare(jgrid, type, x, y, value) {
+// class Location
+function Location(x, y) {
+	this.x = x;
+	this.y = y;
+}
+
+// class Fgrid (full information grid)
+function Fgrid(jgrid, type){
+	// initialization
+	this.jgrid = jgrid;
+	this.type = type;
+	this.valid = null;
+}
+
+Fgrid.prototype.jTd = function(x, y) {
+	return $("td#" + this.type + x + y, this.jgrid);
+}
+
+Fgrid.prototype.jTdInput = function(x, y) {
+	return $("input", this.jTd(x, y));
+}
+
+Fgrid.prototype.value = function(x, y, value) {
+	if (value == undefined) {
+		value = parseInt(this.jTdInput(x, y).val());
+		if (IsValidValue(value))
+			return value;
+		else
+			return 0;
+	} else
+		if (IsValidValue(value))
+			this.jTdInput(x, y).val(value);
+		else
+			this.jTdInput(x, y).val("");
+}
+
+Fgrid.prototype.resolved = function() {
+	$(".conflict", this.jgrid).resolved();
+}
+
+Fgrid.prototype.RegisterEditable = function(callback) {
+	var fgrid = this;
+	this.Validate(callback);
+	
+	$("input.gridEmpty", this.jgrid).click(function() {
+		for (var i = 0; i < GridN; i++)
+		for (var j = 0; j < GridN; j++)
+			fgrid.value(i, j, "");
+	});
+	
+	$("td.cell", this.jgrid).each(function(td_index, td) {
+		$("input", td).keydown(function(e) {
+			// only handle arrow key control
+			var id = $(td).attr("id");
+			var loc = new Location(parseInt(id.charAt(1)), parseInt(id.charAt(2)));
+			
+			switch (e.which) {
+				case 37:	// left
+					MoveInBoundary(loc, 0, -1);
+					break;
+				case 38:	// up
+					MoveInBoundary(loc, -1, 0);
+					break;
+				case 39:	// right
+					MoveInBoundary(loc, 0, 1);
+					break;
+				case 40:	// down
+					MoveInBoundary(loc, 1, 0);
+					break;
+				default:
+					return;
+			}
+			e.preventDefault();						// left and right arrow key works
+			$(this).blur();
+			fgrid.jTdInput(loc.x, loc.y).focus();
+		}).keyup(function() {						// change event is only triggered when the input loses focus, so use keyup instead
+			fgrid.Validate(callback);				// we must check the whole grid every time
+			$(this).focus();
+		}).focus(function() {
+			$(this).select();
+		});
+	});
+}
+
+Fgrid.prototype.Register = function() {
+	$("input.gridEmpty", this.jgrid).attr("disabled", "disabled");
+}
+
+// Validate functions
+Fgrid.prototype.Validate = function(callback) {
+	this.resolved();
+	var valid = true;
+	
+	for (var i = 0; i < GridN; i++)
+	for (var j = 0; j < GridN; j++) {
+		if (!this.jTd(i, j).hasClass(".conflict"))
+			if (!this.ValidateCell(i, j))
+				valid = false;
+	}
+	
+	if (valid != this.valid) {
+		this.valid = valid;
+		if (typeof callback == "function")
+			callback(valid);
+	}
+	
+	return valid;
+}
+
+Fgrid.prototype.ValidateRow = function(x, y) {
+	var valid = true;
+	var value = this.value(x, y);
+	
+	for (var j = 0; j < GridN; j++)
+		if (j != y && this.value(x, j) == value) {
+			this.jTd(x, j).conflict();
+			valid = false;
+		}
+	return valid;
+}
+
+Fgrid.prototype.ValidateCol = function(x, y) {
+	var valid = true;
+	var value = this.value(x, y);
+	
+	for (var i = 0; i < GridN; i++)
+		if (i != x && this.value(i, y) == value) {
+			this.jTd(i, y).conflict();
+			valid = false;
+		}
+	return valid;
+}
+
+Fgrid.prototype.ValidateSquare = function(x, y) {
+	var valid = true;
+	var value = this.value(x, y);
 	var si = parseInt(x / 3) * 3;
 	var sj = parseInt(y / 3) * 3;
 	
-	for (i = si; i < si + 3; i++)
-	for (j = sj; j < sj + 3; j++)
-		if ((i != x || j != y) && $(TdInput(type, i, j), jgrid).val() == value)
-			return false;
-	return true;
-}
-
-function IsValidCell(jgrid, type, x, y, value) {
-	if (value == "")
-		return true;
-	else
-		return	IsValidForRow(jgrid, type, x, y, value) &&
-				IsValidForCol(jgrid, type, x, y, value) &&
-				IsValidForSquare(jgrid, type, x, y, value);
-}
-
-function ValidateJgrid(jgrid, type) {
-	$("td.conflict", jgrid).removeClass("conflict");
-	
-	var valid = true;
-	for (x = 0; x < GridN; x++)
-	for (y = 0; y < GridN; y++) {
-		var value = $(TdInput(type, x, y), jgrid).val();
-		if (!IsValidCell(jgrid, type, x, y, value)) {
-			$(Td(type, x, y), jgrid).addClass("conflict");
+	for (var i = si; i < si + 3; i++)
+	for (var j = sj; j < sj + 3; j++)
+		if ((i != x || j != y) && this.value(i, j) == value) {
+			this.jTd(i, j).conflict();
 			valid = false;
 		}
-	}
+	return valid;
+}
+
+Fgrid.prototype.ValidateCell = function(x, y) {
+	if (this.value(x, y) == 0)
+		return true;
+	
+	var valid =	this.ValidateRow(x, y) &&
+				this.ValidateCol(x, y) &&
+				this.ValidateSquare(x, y);
+	
+	if (!valid)
+		this.jTd(x, y).conflict();
 	return valid;
 }
